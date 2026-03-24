f0f28486c64fd10fa4e2a94d613c28be1647060d
Viktor226 <vanistratenko.a.5@gmail.com>
2026-03-24 14:10:52 +0200

    Fix typo in animal attribute from 'lakomstwo' to 'лакомство'

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ if st.session_state.animals:
             st.subheader(animal["име"])
             st.image(animal["картинка"], use_column_width=True)
             st.write(animal["описание"])
-            st.write(animal["lakomstwo"])
+            st.write(animal["лакомство"])
 else:
     st.info("Галерията е празна. Добавете животни!")
 
